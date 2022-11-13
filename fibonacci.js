3e93dd41b51c1606f0e2a77afc563f7a10d5e7de
Bryan Miller <bmilcs@yahoo.com>
2022-11-13 10:00:56 -0500

    Add print function, comments & better naming conventions

--- fibonacci.js
+++ fibonacci.js
@@ -1,38 +1,42 @@
-// FIBONACCI SEQUENCE
-
-console.log("Fibonacci Sequence\nAssignment #1");
+console.log("\nfibonacci sequence\nassignment #1\n");
 
 //
-// Function #1: Using Iteration
+// Function #1: Iterative Fibonacci
 //
 
-const fibsIteration = (num) => {
+const fibIteration = (num) => {
   // create an array
   let arr = [];
-  // loop "number" of times
+  // loop "num" times
   for (let i = 0; i < num; i++) {
-    // if i = 0 or 1, push those to array
+    // if 0 or 1, push value to array
     if (i === 0 || i === 1) {
       arr.push(i);
       continue;
     }
-    // else push fib[i-2] + fib[i-1]
+    // else push last 2 numbers: fib[i-2] + fib[i-1]
     arr.push(arr[i - 2] + arr[i - 1]);
   }
-  // return array
+  // return final result
   return arr;
 };
 
 //
-// Function #2: Using Recursion
+// Function #2: Recursive Fibonacci
 //
 
-const fibsRecursive = (num, arr = []) => {
-  if (num === 0) {
-    return arr;
-  } else if (arr.length === 0) return fibsRecursive(num - 2, [0, 1]);
+const fibRecursive = (num, arr = []) => {
+  // base case: if num is zero, return array
+  if (num === 0) return arr;
+  // if arr.length is zero (the original function call)
+  // recursively call the function with the minimum data needed
+  // to perform the fibonacci sequence & subtract 2 from "num" count:
+  else if (arr.length === 0) return fibRecursive(num - 2, [0, 1]);
   else {
-    return fibsRecursive(--num, [
+    // else recursively call the function with num - 1,
+    // spreading the original array out & adding the
+    // sum of the last 2 numbers to the end of the array
+    return fibRecursive(--num, [
       ...arr,
       arr[arr.length - 2] + arr[arr.length - 1],
     ]);
@@ -40,15 +44,32 @@ const fibsRecursive = (num, arr = []) => {
 };
 
 //
-// Print function
+// Print functions
 //
 
-const printFib = (number, callback) => {
-  console.log(`\nInput: ${number}`);
-  console.log(callback(number));
+const printTitle = (title) => {
+  console.log("-----------------------------");
+  console.log(title);
+  console.log("-----------------------------\n");
 };
 
-printFib(5, fibsIteration);
-printFib(5, fibsRecursive);
-printFib(8, fibsIteration);
-printFib(8, fibsRecursive);
+const printFib = (number, callback) => {
+  console.log(`in:\t${number}`);
+  console.log(`out:\t${callback(number)}\n`);
+};
+
+//
+// Sample Tests
+//
+
+printTitle("iterative fibonacci:");
+
+printFib(5, fibIteration);
+printFib(8, fibIteration);
+printFib(15, fibIteration);
+
+printTitle("recursive fibonacci:");
+
+printFib(5, fibRecursive);
+printFib(8, fibRecursive);
+printFib(15, fibRecursive);
